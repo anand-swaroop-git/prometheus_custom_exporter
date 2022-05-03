f1800bf5ef07298d1192e882462c16697dd0b0ee
anand-swaroop <anand.swaroop@deferit.com>
2022-05-03 17:52:52 +0930

    Add custom labels and only keep gauge metric.

--- collector.py
+++ collector.py
@@ -4,7 +4,7 @@ from os import path
 import yaml
 from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
 from prometheus_client import start_http_server
-totalRandomNumber = 0
+
 class RandomNumberCollector(object):
     def __init__(self):
         pass
@@ -12,11 +12,6 @@ class RandomNumberCollector(object):
         gauge = GaugeMetricFamily("random_number", "A random number generator", labels=["randomNum"])
         gauge.add_metric(['random_num'], random.randint(1, 20))
         yield gauge
-        count = CounterMetricFamily("random_number_2", "A random number 2.0", labels=['randomNum'])
-        global totalRandomNumber
-        totalRandomNumber += random.randint(1,30)
-        count.add_metric(['random_num'], totalRandomNumber)
-        yield count
 
 if __name__ == "__main__":
     start_http_server(9000)
