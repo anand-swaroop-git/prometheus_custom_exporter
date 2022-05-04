4c7b1389afbd91aa8a295a8c68a0a8173bfab795
anand-swaroop <anand.swaroop@deferit.com>
2022-05-04 15:08:03 +0930

    Working POC. Custom disk metrics are being sent through.

--- collector.py
+++ collector.py
@@ -1,21 +1,33 @@
+import os
 import time
-import random
-from os import path
-import yaml
-from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
+from prometheus_client.core import GaugeMetricFamily, REGISTRY
 from prometheus_client import start_http_server
 
-class RandomNumberCollector(object):
-    def __init__(self):
-        pass
+metric = GaugeMetricFamily("disk_encryption", "all system metrics", labels=["encrypted", "disk_name"])
+
+class CustomCollector(object):
+
     def collect(self):
-        gauge = GaugeMetricFamily("random_number", "A random number generator", labels=["randomNum"])
-        gauge.add_metric(['random_num'], random.randint(1, 20))
-        yield gauge
+        def is_encrypted(diskname):
+            # Placeholder logic
+            if diskname.startswith('/'):
+                return 'true'
+            else:
+                return 'false'
+
+        du = os.popen("df -h | awk '{print $1}' | grep -Eiv 'Filesystem|map'")
+        for line in du:
+            if line: # To avoid empty lines
+                encrypted = is_encrypted(line)
+            else:
+                continue
+            metric.add_metric([encrypted, line.strip()], 1)
+
+        yield metric
 
 if __name__ == "__main__":
     start_http_server(9000)
-    REGISTRY.register(RandomNumberCollector())
+    REGISTRY.register(CustomCollector())
     while True: 
         # period between collection
-        time.sleep(1)
+        time.sleep(5)
